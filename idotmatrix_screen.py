edc8aa8a9841e657d06a6569dade32ce04611c95
Konrad Kosmowski <kos@kosmosik.net>
2026-03-04 22:06:33 +0100

    added some stuff

---
+++ idotmatrix_screen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import asyncio
+import argparse
+
+from idotmatrix import ConnectionManager
+
+parser = argparse.ArgumentParser()
+parser.add_argument("mac")
+parser.add_argument("status", choices=['on', 'off'])
+args = parser.parse_args()
+
+async def main(mac, status):
+    conn = ConnectionManager()
+    data = { "on": bytearray([5,0,7,1,1]), "off": bytearray([5,0,7,1,0]) }
+    await conn.connectByAddress(mac)
+    await conn.send(data=data[status])
+    await conn.disconnect()
+
+asyncio.run(main(args.mac, args.status))
