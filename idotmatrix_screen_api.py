a9650d186330e9076184ceee114c96f0d3143c01
Konrad Kosmowski <kos@kosmosik.net>
2026-03-05 10:26:24 +0100

    updated api to use search instead of fixed mac

--- idotmatrix_screen_api.py
+++ idotmatrix_screen_api.py
@@ -7,17 +7,12 @@ from os import environ
 from fastapi import FastAPI
 from idotmatrix import ConnectionManager
 
-IDMMAC = environ.get("IDMMAC")
-if not IDMMAC:
-    print("FATAL ERROR: IDMMAC environment variable is not set.", file=sys.stderr)
-    sys.exit(1)
 
-
-async def idm_screen(mac, state):
+async def idm_screen(state):
     try:
         conn = ConnectionManager()
         data = {"on": bytearray([5, 0, 7, 1, 1]), "off": bytearray([5, 0, 7, 1, 0])}
-        await conn.connectByAddress(mac)
+        await conn.connectBySearch()
         await conn.send(data=data[state])
         await conn.disconnect()
     except Exception as e:
@@ -31,9 +26,9 @@ app = FastAPI()
 
 @app.get("/idm_on")
 async def turn_on():
-    return await idm_screen(IDMMAC, "on")
+    return await idm_screen("on")
 
 
 @app.get("/idm_off")
 async def turn_off():
-    return await idm_screen(IDMMAC, "off")
+    return await idm_screen("off")
